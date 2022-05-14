a78652f8900332d7dfa6aa569182a115a1ffdacf
tutu-kang <123kangpeng@gmail.com>
2022-05-14 12:17:16 -0500

    add stable marriage algorithm

--- CustomSchedule/src/main/java/sys/cloud/matchingscheduler/MatchingScheduler.java
+++ CustomSchedule/src/main/java/sys/cloud/matchingscheduler/MatchingScheduler.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.Map.Entry;
 import java.util.Map;
 import java.util.Set;
+import java.util.Iterator;
 
 import org.apache.storm.generated.Bolt;
 import org.apache.storm.generated.ComponentCommon;
@@ -696,8 +697,9 @@ public class MatchingScheduler implements IScheduler {
 
     private List<WorkerSlot> TwoSideMatching(Cluster cluster,
                                              Map<WorkerSlot, Pair<String, ArrayList<ExecutorDetails>>> assignments,
-                                             HashMap<String, ArrayList<ArrayList<ExecutorDetails>>> executorByContainerForLayer,
+                                             HashMap<String, ArrayList<ArrayList<ExecutorDetails>>> executorByContainerForCurrentLayer,
                                              List<WorkerSlot> allAvailableSlots, List<WorkerSlot> allocatedSlots)
+            // ExecutorByContainerForLayer for each layer.
     {
         // Microservices will prefer the cheapest resoruces. Microserivces also want to spend less money.
         // resrouce preference is the microservice request smalledst resource. Each resource want to hold as much as possible microservices.
@@ -715,13 +717,34 @@ public class MatchingScheduler implements IScheduler {
             String message = "No slots are available for assigning executors for (components)";
             LOG.error("Peng " + message);
         }
+        // create the preference for container
+        ArrayList<Pair<String, ArrayList<ExecutorDetails>>> groupExecutorPreference = new ArrayList<Pair<String, ArrayList<ExecutorDetails>>>();
+
+        // create the preference for worker slot.
+        int[][] men = {
+                {0, 1, 2, 3},
+                {0, 1, 2, 3},
+                {0, 1, 2, 3},
+                {0, 1, 2, 3}
+        };
+
+        // Preference order for 3 women
+        int[][] women = {
+                {1, 0, 2, 3},
+                {1, 2, 3, 0},
+                {0, 1, 3, 2},
+                {0, 1, 3, 2}
+        };
+        StableMarriage sm = new StableMarriage();
+        sm.matchMaking(men, women);
+
 
         // Divide the executors evenly across the slots and get a map of slot to executors
         // using two side matching algorithm
         //Map<WorkerSlot, ArrayList<ExecutorDetails>> assignments = new HashMap<WorkerSlot, ArrayList<ExecutorDetails>>();
 
         int currentSlotIndex = 0;
-        for (Entry<String, ArrayList<ArrayList<ExecutorDetails>>> entry : executorByContainerForLayer.entrySet()) {
+        for (Entry<String, ArrayList<ArrayList<ExecutorDetails>>> entry : executorByContainerForCurrentLayer.entrySet()) {
             String topologyID = entry.getKey();
             ArrayList<ArrayList<ExecutorDetails>> executorsToAssignList = entry.getValue();
 
@@ -754,28 +777,6 @@ public class MatchingScheduler implements IScheduler {
 
     }
 
-    private void MatchingSchedule2(Topologies topologies, Cluster cluster) {
-        Collection<SupervisorDetails> supervisorDetails = cluster.getSupervisors().values();
-        LOG.info(supervisorDetails.toString());
-        // get the supervisor's configuration information.
-        Map<String, ArrayList<SupervisorDetails>> supervisorConfInfo = getSupervisorsConfiguration(supervisorDetails);
-        //1. define allocation array
-        //2. sort the application by end to end latency requirement
-        //3. for topology in topologies_list:
-        //4.    while True do until finding the satisfied configurations.
-        //5.        alloc = two side matching(app)
-        //6.        if check_allocation(alloc) is satifisy:
-        //7.            set status
-        //8.        else:
-        //9.            remove the smallestNode from resource List
-
-        //1. group each topology by our configuration.
-        //2. combine all topology as a forest.
-        //3. for each layer, using matching algorithm. container prefer resource. resource prefer less usage for bandwidth.
-        //3.1 for the intermediate layer, we need to consider the predecessor container.
-
-    }
-
     @Override
     @SuppressWarnings("rawtypes")
     public void prepare(Map conf) {
@@ -788,5 +789,99 @@ public class MatchingScheduler implements IScheduler {
     @Override
     public void schedule(Topologies topologies, Cluster cluster) {
         MatchingSchedule(topologies, cluster);
+    }
+}
+
+class StableMarriage {
+    private static final Logger LOG = LoggerFactory.getLogger(MatchingScheduler.class);
+
+    public void matchMaking(int [][] men, int [][] women){
+        HashMap<Integer, Integer> couples = findCouples(men, women);
+        LOG.info("\n------------------Final Matching----------------------------");
+        Set<Integer> set = couples.keySet();
+        Iterator<Integer> iterator = set.iterator();
+        while (iterator.hasNext()){
+            int key = iterator.next();
+            LOG.info("Woman: " + key + " is engaged with man: " + couples.get(key));
+        }
+    }
+
+    public HashMap<Integer, Integer> findCouples(int [][] men, int [][] women){
+
+        //couples map will contain all the matches, with women as key and her match (men) as value
+        HashMap<Integer, Integer> couples = new HashMap<>();
+
+        //add all the women to couples map with their matches as NULL (initially)
+        for (int i = 0; i <women.length ; i++) {
+            couples.put(i, null);
+        }
+
+        //create a list of all bachelors
+        Set<Integer> bachelors = new HashSet<>();
+        for (int i = 0; i <men.length ; i++) {
+            bachelors.add(i);
+        }
+
+        int bachelorCount = bachelors.size();
+
+        //do till all the bachelors are nor engaged
+        while(bachelorCount>0){
+
+            int currentBachelor = bachelors.iterator().next();
+            //System.out.println ("\nMan " + currentBachelor + " is looking for a woman now-");
+
+            // check for all the women preferences of current bachelor in preference order
+            for (int wmen = 0; wmen <men[currentBachelor].length ; wmen++) {
+
+                //check if current woman is available for current bachelor
+                if(couples.get(wmen)==null){
+                    //this woman is available for this man, make the match
+                    couples.put(wmen, currentBachelor);
+                    //System.out.println("Women " + wmen + " has ACCEPTED the man: " + currentBachelor);
+                    bachelors.remove(currentBachelor);
+                    break;
+                }else{
+                    //current woman had already accepted the proposal from some other man
+                    //check if women is interested accepting current bachelor
+                    // and dumping the man which she had accepted earlier
+                    int alreadyAcceptedMan = couples.get(wmen);
+                    if(willChangePartner(currentBachelor, alreadyAcceptedMan, wmen, women)){
+
+                        //current women will accept
+                        couples.put(wmen, currentBachelor);
+                        // add the dumped man in bachelor list
+                        bachelors.add(alreadyAcceptedMan);
+                        bachelors.remove(currentBachelor);
+                        //System.out.println("Women " + wmen + " has DUMPED the man: " + alreadyAcceptedMan);
+                        //System.out.println("Women " + wmen + " has ACCEPTED the man: " + currentBachelor);
+                        break; //
+                    }
+                }
+            }
+            //get the size again
+            bachelorCount = bachelors.size();
+        }
+        //return the couples
+        return couples;
+    }
+
+    boolean willChangePartner(int currentBachelor, int alreadyAcceptedMan, int currentWomen, int [][]women){
+
+        int pref_currentBachelor = -1;
+        int pref_alreadyAcceptedMan = -1;
+
+        //get the preferences of both the men
+        for (int i = 0; i <women[currentWomen].length ; i++) {
+
+            if(women[currentWomen][i]==currentBachelor)
+                pref_currentBachelor = i;
+
+            if(women[currentWomen][i]==alreadyAcceptedMan)
+                pref_alreadyAcceptedMan = i;
+        }
+
+        //women will accept the current bachelor only if he has higher preference
+        //than the man she had accepted earlier
+        return pref_currentBachelor < pref_alreadyAcceptedMan;
     }
 }
