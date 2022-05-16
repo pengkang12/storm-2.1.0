34882457ef77b3ea88e573a479ad231d73ac8c36
tutu-kang <123kangpeng@gmail.com>
2022-05-16 11:45:16 -0500

    sperate class to different file

---
+++ CustomSchedule/src/main/java/sys/cloud/matchingscheduler/Pair.java
@@ -0,0 +1,28 @@
+package sys.cloud.matchingscheduler;
+
+public class Pair<A, B> {
+    A first = null;
+    B second = null;
+
+    Pair(A first, B second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public A getFirst() {
+        return first;
+    }
+
+    public void setFirst(A first) {
+        this.first = first;
+    }
+
+    public B getSecond() {
+        return second;
+    }
+
+    public void setSecond(B second) {
+        this.second = second;
+    }
+
+}
---
+++ CustomSchedule/src/main/java/sys/cloud/matchingscheduler/StableMarriage.java
@@ -0,0 +1,87 @@
+package sys.cloud.matchingscheduler;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+public class StableMarriage {
+
+    public HashMap<Integer, Integer> findCouples(int [][] men, int [][] women){
+
+        //couples map will contain all the matches, with women as key and her match (men) as value
+        HashMap<Integer, Integer> couples = new HashMap<>();
+
+        //add all the women to couples map with their matches as NULL (initially)
+        for (int i = 0; i <women.length ; i++) {
+            couples.put(i, null);
+        }
+
+        //create a list of all bachelors
+        Set<Integer> bachelors = new HashSet<>();
+        for (int i = 0; i <men.length ; i++) {
+            bachelors.add(i);
+        }
+
+        int bachelorCount = bachelors.size();
+
+        //do till all the bachelors are nor engaged
+        while(bachelorCount>0){
+
+            int currentBachelor = bachelors.iterator().next();
+            //System.out.println ("\nMan " + currentBachelor + " is looking for a woman now-");
+
+            // check for all the women preferences of current bachelor in preference order
+            for (int wmen = 0; wmen <men[currentBachelor].length ; wmen++) {
+
+                //check if current woman is available for current bachelor
+                if(couples.get(wmen)==null){
+                    //this woman is available for this man, make the match
+                    couples.put(wmen, currentBachelor);
+                    //System.out.println("Women " + wmen + " has ACCEPTED the man: " + currentBachelor);
+                    bachelors.remove(currentBachelor);
+                    break;
+                }else{
+                    //current woman had already accepted the proposal from some other man
+                    //check if women is interested accepting current bachelor
+                    // and dumping the man which she had accepted earlier
+                    int alreadyAcceptedMan = couples.get(wmen);
+                    if(willChangePartner(currentBachelor, alreadyAcceptedMan, wmen, women)){
+
+                        //current women will accept
+                        couples.put(wmen, currentBachelor);
+                        // add the dumped man in bachelor list
+                        bachelors.add(alreadyAcceptedMan);
+                        bachelors.remove(currentBachelor);
+                        //System.out.println("Women " + wmen + " has DUMPED the man: " + alreadyAcceptedMan);
+                        //System.out.println("Women " + wmen + " has ACCEPTED the man: " + currentBachelor);
+                        break; //
+                    }
+                }
+            }
+            //get the size again
+            bachelorCount = bachelors.size();
+        }
+        //return the couples
+        return couples;
+    }
+
+    boolean willChangePartner(int currentBachelor, int alreadyAcceptedMan, int currentWomen, int [][]women){
+
+        int pref_currentBachelor = -1;
+        int pref_alreadyAcceptedMan = -1;
+
+        //get the preferences of both the men
+        for (int i = 0; i <women[currentWomen].length ; i++) {
+
+            if(women[currentWomen][i]==currentBachelor)
+                pref_currentBachelor = i;
+
+            if(women[currentWomen][i]==alreadyAcceptedMan)
+                pref_alreadyAcceptedMan = i;
+        }
+
+        //women will accept the current bachelor only if he has higher preference
+        //than the man she had accepted earlier
+        return pref_currentBachelor < pref_alreadyAcceptedMan;
+    }
+}
