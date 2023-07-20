765666a2133103e3dfeb1435d362a97b05f65d30
tutu-kang <123kangpeng@gmail.com>
2023-07-20 17:29:25 -0500

    update

--- CustomSchedule/script/restart_storm_server.sh
+++ CustomSchedule/script/restart_storm_server.sh
@@ -5,17 +5,19 @@ rm -rf ~/zookeeper/data
 ~/zookeeper/bin/zkServer.sh start
 
 ps aux | grep nimbus | awk '{print $2}' | xargs kill -9
+ps aux | grep supervisor | awk '{print $2}' | xargs kill -9
 
 rm -rf ~/storm/data
 
 sleep 1
 
 ~/storm/bin/storm nimbus &
+~/storm/bin/storm supervisor &
 
 sleep 5
 
 
-for edge in "worker1" "worker2" "edge1" "edge2" "edge3" "edge4" "edge5"
+for edge in "core" "worker1" "worker2" "edge1" "edge2" "edge3" "edge4" "edge5"
 do
 ssh $edge bash kill_storm.sh
 sleep 1
